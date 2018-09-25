b440a9b0b4d41451af10e1251f4e27c41f0c872d
Andres Medina Charcape <andres_mch15@hotmail.com>
2018-09-25 02:29:59 -0500

    Segunda Subida

--- ejercicio3/Factura.php
+++ ejercicio3/Factura.php
@@ -5,49 +5,72 @@ class Factura{
         return new class($p_cliente,$p_producto){
             private $cliente;
             private $producto;
-            private const IVA = 0.18;  //IVA 18%
-            private $ImporteBase;  //Dinero  a pagar sin Iva
-            private $importeBruto; //Dinero a pagar 
-            private $cantidad_producto;
-            private $precio_unitario;
+            private const IGV = 1.18;  //IGV 18%
+            private $ImporteNeto;  //Dinero  a pagar sin IGV
+            private $ImporteBruto; //Dinero a pagar con IGV
+            private $dinero_cliente;
+            private $Impuesto;
+            private $cantidad_productos;
+            private $fecha;
 
 
             public function __construct($p_cliente,$p_producto)
             {
                 $this->cliente = $p_cliente;
                 $this->producto = $p_producto;
+                setlocale(LC_ALL,"es_ES.UTF-8");  //Para que soporte el formato de fecha en español
+                date_default_timezone_set ('America/Lima'); //Definir la ona horaria en Lima
+                $this->fecha = strftime("%A, %d de %B de %Y %H:%M");  //Formato de fecha
             }
 
-            public function comprar($p_cantidad,$p_precio_unitario){
-                $this->cantidad_producto = $p_cantidad;
-                $this->precio_unitario = $p_precio_unitario;
-                $this->ImporteBase = $p_cantidad*$p_precio_unitario;
-                $this->ImporteBruto = $this->ImporteBase*self::IVA + $this->ImporteBase;
-
-
+            public function RealizarCompra($p_cantidad,$p_precioUnitario){
+                $this->cantidad_productos = $p_cantidad;
+                $this->ImporteBruto = round($p_cantidad*$p_precioUnitario,3);
+                $this->ImporteNeto = round(($this->ImporteBruto)/self::IGV ,3);
+                $this->Impuesto = round((18/100)*$this->ImporteNeto,3);
             }
 
-
-            public function Imprimir()
+            public function ImprimirFactura()
             {
 
-                echo '<style>';
-                echo ' div{
-                    color:blue;
-                    padding: 0.1em;
-                 }';
-                echo '</style>';
-                echo '<h1> Emitiendo Factura.. </h1>';
-                echo "<div style='text-align:center;'>";
-                echo ' Cliente: ' .($this->cliente) . '<br>';
-                echo ' producto: '.($this->producto) . '<br>';
-                echo ' cantidad: '.($this->cantidad_producto) . '<br>';
-                echo ' precio_unitario ' .($this->precio_unitario) . '<br>';
-                echo ' IVA: ' .(self::IVA) . '<br>';
-                echo  'ImporteBase: ' .($this->ImporteBase) . '<br>';
-                echo  'Impuesto a pagar: ' .( $this->ImporteBase*self::IVA) . '<br>';
-                echo  'ImporteBruto: ' .($this->ImporteBruto) . '<br>';
-                echo '</div>';
+
+                echo '<h1> Inversiones Charcape </h1>';
+
+                echo '<fieldset>
+                    <legend>Datos del Cliente</legend>
+                    <p>Nombre :'.($this->cliente).'</p>
+                    <p>Fecha:'.($this->fecha).' </p>
+                </fieldset>';
+                //echo "<div style='text-align:center;'>";
+
+                echo '<table> 
+                    <thead>
+                        <tr>
+                            <th>Producto</th>
+                            <th>Cantidad</th>
+                            <th>IGV</th>
+                            <th>Impuesto</th>
+                            <th>Importe Neto</th>
+                        </tr>
+                    </thead>
+                    <tfoot>
+                        <tr> 
+                            <td colspan="4" class="th2">Importe Bruto</td>
+                            <td>'.($this->ImporteBruto) .'</td>
+                        </tr>
+                    </tfoot>
+                    <tbody>
+                        <tr>
+                            <td>'. ($this->producto).'</td>
+                            <td>'. ($this->cantidad_productos).'</td>
+                            <td>18%</td>
+                            <td>'. ($this->Impuesto).'</td>
+                            <td>'. ($this->ImporteNeto).'</td>
+                        </tr>
+                    </tbody>
+                </table>';
+                echo '<link rel="stylesheet" href="estilos.css" type="text/css">';
+
             }
         
         };
--- ejercicio3/control_factura.php
+++ ejercicio3/control_factura.php
@@ -2,9 +2,9 @@
 require_once "Factura.php";
 
  $factura = new Factura;
- $cliente1 = $factura->FacturaRecibida('Andres','Chocolate');
- $cliente1->comprar(3,0.80);
- $cliente1->Imprimir();
+ $cliente1 = $factura->FacturaRecibida('Andres Medina Charcape','Chocolate');
+ $cliente1->RealizarCompra(3,0.80);
+ $cliente1->ImprimirFactura();
 
 
 ?>
